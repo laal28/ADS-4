eabbae48b8304c5f3cffa68cd631768ac3dfa443
laal28 <129623228+laal28@users.noreply.github.com>
2024-03-11 19:06:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,14 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  uint r = 0;
+  for ( int i = 0; i < len; ++i )
+    for ( int j = 0; j < len; ++j )
+      r += (arr[i]+arrj[j] == value);
+
+  return r;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  for( int i = 0; i < len; ++i ) 
 }
 int countPairs3(int *arr, int len, int value) {
   return 0;
