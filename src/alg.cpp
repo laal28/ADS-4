554d3dea1e2bef65fcf9e6110488cf3dfd04b0f4
laal28 <129623228+laal28@users.noreply.github.com>
2024-04-29 16:22:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,15 +1,60 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  unsigned int r = 0;
+  int count = 0;
   for ( int i = 0; i < len; ++i )
-    for ( int j = 0; j < len; ++j )
-      r += (arr[i]+arr[j] == value);
+    for ( int j = i+1; j < len; ++j )
+      if ( ( arr[i] + arr[j] == value ) &&
+           ( arr[i] >= 0 && arr[j] >= 0 ) )
+        count++;
+  
+  return count;
+}
 
-  return r;
-}
 int countPairs2(int *arr, int len, int value) {
-  for( int i = 0; i < len; ++i );
+  int count = 0;
+  int rightPos = len - 1;
+  for ( ; arr[right] > value; )
+    rightPos--;
+    
+  for ( int i = 0; i < rightPos; ++i ) {
+    for ( int j = rightPos; j > i; --j ) {
+      if ( ( arr[i] + arr[j] == value ) &&
+           ( arr[i] >= 0 && arr[j] >= 0 ) ) {
+        count++;
+      } else if ( arr[i] + arr[j] < value ) {
+        break;
+      }
+    }
+  }
+  return count;
 }
+
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for ( int i = 0; i < len - 1; ++i ) {
+    int left = i;
+    int right = len;
+    for ( ; left < right - 1; ) {
+      int centre = (right + left) / 2;
+      if (arr[i] + arr[centre] == value) {
+        count++;
+        int temp = centre + 1;
+        for ( ; ( arr[i] + arr[temp] == value ) &&
+                ( temp < right ); temp++ )
+          count++;
+        
+        temp = centre - 1;
+        for ( ; ( arr[i] + arr[temp] == value ) &&
+                ( temp > left); temp-- )
+          count++;
+      
+        break;
+      } else if ( arr[i] + arr[centre] > value ) {
+        right = centre;
+      } else {
+        left = centre;
+      }
+    }
+  }
+  return count;
 }
